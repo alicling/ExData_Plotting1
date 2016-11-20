e453e11f527ebcb24d4d87932228924fdfc99319
alicling <xli30@tulane.edu>
2016-11-19 21:58:11 -0600

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,6 +1,8 @@
+#read the data
 > datafile<-"./data/household_power_consumption.txt"
 > data<-read.table(datafile,header = TRUE,sep=";",stringsAsFactors=FALSE, dec=".")
 > head(data)
+#subsetdata
 > subsetdata<-data[data$Date%in%c("1/2/2007","2/2/2007"),]
 > head(subsetdata)
 > globalactivepower<-as.numeric(subsetdata$Global_active_power)
@@ -8,13 +10,18 @@
 > submetering1<-as.numeric(subsetdata$Sub_metering_1)
 > submetering2<-as.numeric(subsetdata$Sub_metering_2)
 > submetering3<-as.numeric(subsetdata$Sub_metering_3)
+#plot4
 > png("plot4.png", width=480, height=480)
 > par(mfrow = c(2, 2))
+#plot4-1
 > plot(datetime, globalactivepower, type="l", xlab="", ylab="Global Active Power", cex=0.2)
+#plot4-2
 > plot(datetime, subsetdata$Voltage, type="l", xlab="datetime", ylab="Voltage")
+#plot4-3
 > plot(datetime,submetering1,ylab = "Energy sub metering",xlab = "",type = "l")
 > lines(datetime,submetering2,type="l",col="red")
 > lines(datetime,submetering3,type="l",col="blue")
 > legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=1,lwd = 2.5,col = c("black","red","blue"))
+#plot4-4
 > plot(datetime, subsetdata$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
 > dev.off()
