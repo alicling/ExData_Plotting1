bbac9c585dfd9eeaf60eb533cc791772114ef863
alicling <xli30@tulane.edu>
2016-11-19 21:56:58 -0600

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,6 +1,8 @@
+# read the data
 > datafile<-"./data/household_power_consumption.txt"
 > data<-read.table(datafile,header = TRUE,sep=";",stringsAsFactors=FALSE, dec=".")
 > head(data)
+#subsetdata
 > subsetdata<-data[data$Date%in%c("1/2/2007","2/2/2007"),]
 > head(subsetdata)
 > globalactivepower<-as.numeric(subsetdata$Global_active_power)
@@ -8,6 +10,7 @@
 > submetering1<-as.numeric(subsetdata$Sub_metering_1)
 > submetering2<-as.numeric(subsetdata$Sub_metering_2)
 > submetering3<-as.numeric(subsetdata$Sub_metering_3)
+# plot3
 > png("plot3.png", width=480, height=480)
 > plot(datetime,submetering1,ylab = "Energy sub metering",xlab = "",type = "l")
 > lines(datetime,submetering2,type="l",col="red")
