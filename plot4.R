83d759c79ae0a044f35ce2be4c100dc5b07a29d7
alicling <xli30@tulane.edu>
2016-11-19 21:49:05 -0600

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -5,20 +5,16 @@
 > head(subsetdata)
 > globalactivepower<-as.numeric(subsetdata$Global_active_power)
 > datetime <- strptime(paste(subsetdata$Date, subsetdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
-
-
 > submetering1<-as.numeric(subsetdata$Sub_metering_1)
-
 > submetering2<-as.numeric(subsetdata$Sub_metering_2)
-
 > submetering3<-as.numeric(subsetdata$Sub_metering_3)
-
-> png("plot3.png", width=480, height=480)
-
+> png("plot4.png", width=480, height=480)
+> par(mfrow = c(2, 2))
+> plot(datetime, globalactivepower, type="l", xlab="", ylab="Global Active Power", cex=0.2)
+> plot(datetime, subsetdata$Voltage, type="l", xlab="datetime", ylab="Voltage")
 > plot(datetime,submetering1,ylab = "Energy sub metering",xlab = "",type = "l")
-
 > lines(datetime,submetering2,type="l",col="red")
-
 > lines(datetime,submetering3,type="l",col="blue")
-
 > legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=1,lwd = 2.5,col = c("black","red","blue"))
+> plot(datetime, subsetdata$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
+> dev.off()
